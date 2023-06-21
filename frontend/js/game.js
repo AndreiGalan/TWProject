14e27ae0976d856235644a98a9478beb0d287017
AndreiGalan <andreigalan03@gmail.com>
2023-06-22 01:06:25 +0300

    Bug rezolvat cu apelarea functiilor butoanelor + reactulizare pagina cand se trece la urm intrebare

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -1,12 +1,12 @@
-import {getDifficulty,getImage,getEquation,getAnswers} from "./gameHelperAPI.js";
-import {setQuestionText, setAnswers, setEquation, setImage} from "./gameHelperContent.js";
+import { getDifficulty, getImage, getEquation, getAnswers } from "./gameHelperAPI.js";
+import { setQuestionText, setAnswers, setEquation, setImage } from "./gameHelperContent.js";
 
 let questions = [];
 let nrQuestion = 0;
-const buttonFinish = "<button class=\"next-btn\" type=\"button\" onclick=\"location.href='GameOver.html'\">Finish</button>";
-const buttonNext = "<button class=\"next-btn\" type=\"button\" onclick=\"incrementQuestion()\">Next</button>";
-function getQuestions(){
+const buttonFinish = "<button class=\"next-btn\" type=\"button\">Finish</button>";
+const buttonNext = "<button class=\"next-btn\" type=\"button\">Next</button>";
 
+function getQuestions() {
     const difficulty = getDifficulty();
 
     const requestOptions = {
@@ -16,13 +16,12 @@ function getQuestions(){
     //get all questions
     fetch("http://localhost/TWProject/backend/questions/quiz/" + difficulty + "/10", requestOptions)
         .then(response => {
-                    if (response.ok) {
-                        return response.json();
-                    } else if (response.status === 444){ // no questions found
-                        alert('No questions found.');
-                    }
-                }
-        )
+            if (response.ok) {
+                return response.json();
+            } else if (response.status === 444) { // no questions found
+                alert('No questions found.');
+            }
+        })
         .then(result => {
             console.log(result);
             //TODO loop through the questions
@@ -32,17 +31,42 @@ function getQuestions(){
         .catch(error => console.log('error', error));
 }
 
-function printQuestion(nrQuestion){
+function incrementQuestion() {
+    console.log("increment");
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (!selectedAnswer) {
+        alert('Select an answer before proceeding.');
+        return;
+    }
+    nrQuestion = nrQuestion + 1;
+    printQuestion(nrQuestion);;
+}
 
+function finishButtonAction() {
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (!selectedAnswer) {
+        alert('Select an answer before proceeding.');
+        return;
+    }
+    window.location.href='GameOver.html';
+}
+
+function printQuestion(nrQuestion) {
     setQuestionText(questions[nrQuestion]);
 
     const answersPromise = getAnswers(questions[nrQuestion]['id']);
+    //delete the previous answers
+    if(nrQuestion !== 0){
+        console.log("delete");
+        const answersHolder = document.querySelector(".form-answers");
+        answersHolder.innerHTML = "";
+    }
     answersPromise.then(answers => {
         setAnswers(answers);
     });
 
     //check the type of the question
-    if (questions[nrQuestion]['id_picture'] === null){
+    if (questions[nrQuestion]['id_picture'] === null) {
         //it is an equation
         //get the equation
         const equationPromise = getEquation(questions[nrQuestion]['id_equation']);
@@ -51,8 +75,7 @@ function printQuestion(nrQuestion){
             setEquation(equation);
         });
 
-    }
-    else{
+    } else {
         //it is a question with an image
         //get the link of the image
         const imagePromise = getImage(questions[nrQuestion]['id_picture']);
@@ -62,24 +85,22 @@ function printQuestion(nrQuestion){
         });
     }
 
-    if(nrQuestion === questions.length - 1){
-        //it is the last question
-        document.querySelector("form").innerHTML += buttonFinish;
+    const form = document.querySelector("form");
+    form.innerHTML += (nrQuestion === questions.length - 1) ? buttonFinish : buttonNext;
+
+    // Add event listener to the "Next" button
+    const nextButton = form.querySelector(".next-btn");
+    nextButton.addEventListener("click", incrementQuestion);
+
+
+
+    if (nrQuestion === questions.length - 1) {
+        // Remove the "Next" button if it's the last question
+        nextButton.remove();
+        console.log("finish");
+        const finishButton = form.querySelector(".next-btn");
+        finishButton.addEventListener("click", finishButtonAction);
     }
-    else{
-        document.querySelector("form").innerHTML += buttonNext;
-    }
-
-}
-
-function incrementQuestion(){
-    console.log("increment");
-    nrQuestion= nrQuestion + 1;
-    printQuestion(nrQuestion);
-}
-
-function printToConsole(){
-    console.log(questions);
 }
 
 //main
--- frontend/js/gameHelperContent.js
+++ frontend/js/gameHelperContent.js
@@ -1,44 +1,44 @@
-/**
- * Set the question text in the HTML for a given question
- * @param question
- */
-export function setQuestionText(question) {
-    let questionText = "<h2>" + question['question_text'] +"</h2>";
+    /**
+     * Set the question text in the HTML for a given question
+     * @param question
+     */
+    export function setQuestionText(question) {
+        let questionText = "<h2>" + question['question_text'] +"</h2>";
 
-    const questionHolder = document.querySelector(".question-holder");
-    questionHolder.innerHTML = questionText;
-}
-
-/**
- * Set the answers in the HTML
- * @param answers
- */
-export function setAnswers(answers) {
-
-    const answersHolder = document.querySelector(".form-answers");
-
-    for(let i = 0; i < answers.length; i++){
-        let input = "<input type=\"radio\" id=\"answer" + i + "\" name=\"answer\" value=\"answer" + i + "\">";
-        let span = "<span class=\"alphabet\" > " + String.fromCharCode(65 + i) + "</span>"+ answers[i]['answer_text'];
-        let label = "<label for=\"answer" + i + "\">" + input + span + "</label>";
-        answersHolder.innerHTML += label;
+        const questionHolder = document.querySelector(".question-holder");
+        questionHolder.innerHTML = questionText;
     }
-}
 
-/**
- * Set the equation in the HTML
- * @param equation
- */
-export function setEquation(equation) {
-    const equationHolder = document.querySelector(".image-equation");
-    equationHolder.innerHTML = equation['equation_text'];
-}
+    /**
+     * Set the answers in the HTML
+     * @param answers
+     */
+    export function setAnswers(answers) {
 
-/**
- * Set the image in the HTML
- * @param image
- */
-export function setImage(image) {
-    const imageHolder = document.querySelector(".image-equation");
-    imageHolder.innerHTML = "<img src=\"" + image['download_link'] + "\" alt=\"image\">";
-}
+        const answersHolder = document.querySelector(".form-answers");
+
+        for(let i = 0; i < answers.length; i++){
+            let input = "<input type=\"radio\" id=\"answer" + i + "\" name=\"answer\" value=\"answer" + i + "\">";
+            let span = "<span class=\"alphabet\" > " + String.fromCharCode(65 + i) + "</span>"+ answers[i]['answer_text'];
+            let label = "<label for=\"answer" + i + "\">" + input + span + "</label>";
+            answersHolder.innerHTML += label;
+        }
+    }
+
+    /**
+     * Set the equation in the HTML
+     * @param equation
+     */
+    export function setEquation(equation) {
+        const equationHolder = document.querySelector(".image-equation");
+        equationHolder.innerHTML = equation['equation_text'];
+    }
+
+    /**
+     * Set the image in the HTML
+     * @param image
+     */
+    export function setImage(image) {
+        const imageHolder = document.querySelector(".image-equation");
+        imageHolder.innerHTML = "<img src=\"" + image['download_link'] + "\" alt=\"image\">";
+    }
