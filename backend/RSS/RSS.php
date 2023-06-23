fe389710a342e525030e3c07b74f79a51026605a
Gabriel <gabriel.ignat.v09@gmail.com>
2023-06-23 12:20:16 +0300

    rss updated to 2.0 specifications

--- backend/RSS/RSS.php
+++ backend/RSS/RSS.php
@@ -19,12 +19,13 @@ class RSS {
             $dom->appendChild($rss);
 
             $channel = $dom->createElement("channel");
+
             $rss->appendChild($channel);
 
             $title = $dom->createElement("title", "Top 10 Users");
             $channel->appendChild($title);
 
-            $link = $dom->createElement("link", "/RSS/feed.xml");
+            $link = $dom->createElement("link", "http://localhost/TWProject/RSS/feed.xml");
             $channel->appendChild($link);
 
             $description = $dom->createElement("description", "Top 10 Users with the most points");
@@ -33,13 +34,13 @@ class RSS {
             $language = $dom->createElement("language", "en-us");
             $channel->appendChild($language);
 
-            // publish date
-            $pubDate = $dom->createElement("pubDate", date("Y-m-d\TH:i:s\Z"));
+            // publish date -> element must be an RFC-822 date-time
+            $pubDate = $dom->createElement("pubDate", date("D, d M Y H:i:s O"));
             $channel->appendChild($pubDate);
 
             // author
-            $author = $dom->createElement("author", "Fruits on the Web");
-            $channel->appendChild($author);
+            $managingEditor = $dom->createElement("managingEditor", "fruitsonthewebcontact@gmail.com");
+            $channel->appendChild($managingEditor);
 
             // generator
             $generator = $dom->createElement("generator", "Fruits on the Web");
@@ -52,19 +53,34 @@ class RSS {
 
             foreach ($users as $user) {
                 $item = $dom->createElement("item");
+                //                <guid>unique-identifier</guid>
+                $guid = $dom->createElement("guid", $user['ranking']);
+                $item->appendChild($guid);
+
                 $channel->appendChild($item);
 
-                $position = $dom->createElement("position", $user['ranking']);
-                $item->appendChild($position);
+                $title = $dom->createElement("title", $user['username']);
+                $item->appendChild($title);
 
-                $username = $dom->createElement("username", $user['username']);
-                $item->appendChild($username);
+                $position = $user['ranking'];
+                $username = $user['username'];
+                $points = $user['points'];
+                $playingSince = $user['created_at'];
 
-                $points = $dom->createElement("points", $user['points']);
-                $item->appendChild($points);
 
-                $playingSince = $dom->createElement("playingSince", $user['created_at']);
-                $item->appendChild($playingSince);
+
+                $contentEncoded = $dom->createElement("content");
+                $contentEncoded->setAttribute("type", "html");
+                $content = $dom->createTextNode(
+                    "<p>Position: $position</p>
+                    <p>Username: $username</p>
+                    <p>Points: $points</p>
+                    <p>Playing since: $playingSince</p>"
+                );
+
+                $contentEncoded->appendChild($content);
+
+                $item->appendChild($contentEncoded);
             }
 
             $dom->save("../RSS/feed.xml");
